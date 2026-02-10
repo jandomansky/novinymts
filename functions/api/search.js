b19b58071552760d709a89b0718a64e085f67f76
jandomansky <jan.domansky@metrostav.cz>
2026-02-10 14:05:02 +0100

    Update search.js

--- functions/api/search.js
+++ functions/api/search.js
@@ -6,18 +6,18 @@ export async function onRequestGet({ env, request }) {
 
     if (!q) return Response.json({ ok: true, q: "", results: [] });
 
-    const sql = `
-      SELECT
-        p.rowid AS id,
-        p.title AS title,
-        p.url   AS url,
-        p.date  AS date,
-        snippet(pages_fts, 0, '<mark>', '</mark>', '…', 12) AS snippet
-      FROM pages_fts
-      JOIN pages p ON p.rowid = pages_fts.rowid
-      WHERE pages_fts MATCH ?
-      LIMIT ?;
-    `;
+const sql = `
+  SELECT
+    p.rowid AS id,
+    p.title AS title,
+    p.url   AS url,
+    snippet(pages_fts, 0, '<mark>', '</mark>', '…', 12) AS snippet
+  FROM pages_fts
+  JOIN pages p ON p.rowid = pages_fts.rowid
+  WHERE pages_fts MATCH ?
+  LIMIT ?;
+`;
+
 
     const { results } = await env.DB.prepare(sql).bind(q, limit).all();
     return Response.json({ ok: true, q, results });
